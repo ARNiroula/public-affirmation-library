71e5f299585f4147f0ade698fd5b58d5379af0ff
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-03-29 15:43:41 -0400

    Init DDL Script for Postgres

---
+++ data-model/Postgres_DDL.sql
@@ -0,0 +1,794 @@
+-- SQLINES DEMO *** le SQL Developer Data Modeler 24.3.1.351.0831
+-- SQLINES DEMO *** -03-29 15:30:41 EDT
+-- SQLINES DEMO *** le Database 21c
+-- SQLINES DEMO *** le Database 21c
+
+
+
+-- SQLINES DEMO *** no DDL - MDSYS.SDO_GEOMETRY
+
+-- SQLINES DEMO *** no DDL - XMLTYPE
+
+-- SQLINES FOR EVALUATION USE ONLY (14 DAYS)
+CREATE TABLE AYT_AUTHOR 
+    ( 
+     AUTH_ID    INT  NOT NULL , 
+     FNAME      VARCHAR (30)  NOT NULL , 
+     MNAME      VARCHAR (10) , 
+     LNAME      VARCHAR (30) , 
+     ADDR_ST    VARCHAR (100) , 
+     ADDR_CITY  VARCHAR (100) , 
+     ADDR_STATE VARCHAR (100) , 
+     ADDR_CTRY  VARCHAR (100) , 
+     EMAIL      VARCHAR (254) 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_AUTHOR.AUTH_ID IS 'Unique Identifier for the Author' 
+;
+
+COMMENT ON COLUMN AYT_AUTHOR.FNAME IS 'First Name of the Author' 
+;
+
+COMMENT ON COLUMN AYT_AUTHOR.MNAME IS 'Middle Initials of the Author' 
+;
+
+COMMENT ON COLUMN AYT_AUTHOR.LNAME IS 'Last Name of the Author' 
+;
+
+COMMENT ON COLUMN AYT_AUTHOR.ADDR_ST IS 'Mailing Address of the Author' 
+;
+
+COMMENT ON COLUMN AYT_AUTHOR.ADDR_CITY IS 'Mailing City of the author' 
+;
+
+COMMENT ON COLUMN AYT_AUTHOR.ADDR_STATE IS 'Mailing State of the author' 
+;
+
+COMMENT ON COLUMN AYT_AUTHOR.ADDR_CTRY IS 'Mailing Country of the Author' 
+;
+
+COMMENT ON COLUMN AYT_AUTHOR.EMAIL IS 'Email of the Author' 
+;
+
+ALTER TABLE AYT_AUTHOR 
+    ADD CONSTRAINT AYT_AUTHOR_PK PRIMARY KEY ( AUTH_ID ) ;
+
+CREATE TABLE AYT_AUTHOR_SEMINAR 
+    ( 
+     INVITATION_ID BIGINT  NOT NULL , 
+     AUTH_ID       INT , 
+     EVENT_ID      BIGINT  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_AUTHOR_SEMINAR.INVITATION_ID IS 'Invitation ID for the Seminar' 
+;
+
+ALTER TABLE AYT_AUTHOR_SEMINAR 
+    ADD CONSTRAINT AYT_AUTHOR_SEMINAR_PK PRIMARY KEY ( INVITATION_ID ) ;
+
+CREATE TABLE AYT_BOOK 
+    ( 
+     BOOK_ID  INT  NOT NULL , 
+     ISBN     VARCHAR (13)  NOT NULL , 
+     NAME     VARCHAR (200)  NOT NULL , 
+     TOPIC    VARCHAR (50)  NOT NULL , 
+     PUB_DATE TIMESTAMP(0)  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_BOOK.BOOK_ID IS 'Unique Identifier of the book' 
+;
+
+COMMENT ON COLUMN AYT_BOOK.ISBN IS 'ISBN Number of the Book' 
+;
+
+COMMENT ON COLUMN AYT_BOOK.NAME IS 'Name of the Book' 
+;
+
+COMMENT ON COLUMN AYT_BOOK.TOPIC IS 'Topic the book falls under' 
+;
+
+COMMENT ON COLUMN AYT_BOOK.PUB_DATE IS 'Date the Book was published' 
+;
+
+ALTER TABLE AYT_BOOK 
+    ADD CONSTRAINT AYT_BOOK_PK PRIMARY KEY ( BOOK_ID ) ;
+
+CREATE TABLE AYT_BOOK_AUTHOR 
+    ( 
+     AUTH_ID INT  NOT NULL , 
+     BOOK_ID INT  NOT NULL 
+    ) 
+;
+
+ALTER TABLE AYT_BOOK_AUTHOR 
+    ADD CONSTRAINT AYT_BOOK_AUTHOR_PK PRIMARY KEY ( BOOK_ID, AUTH_ID ) ;
+
+CREATE TABLE AYT_BOOK_COPY 
+    ( 
+     COPY_ID INT  NOT NULL , 
+     BOOK_ID INT  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_BOOK_COPY.COPY_ID IS 'Unique Identifier for Copy of the Book' 
+;
+
+ALTER TABLE AYT_BOOK_COPY 
+    ADD CONSTRAINT AYT_BOOK_COPY_PK PRIMARY KEY ( COPY_ID ) ;
+
+CREATE TABLE AYT_CUST 
+    ( 
+     CUST_ID INTEGER  NOT NULL , 
+     FNAME   VARCHAR (30)  NOT NULL , 
+     LNAME   VARCHAR (30)  NOT NULL , 
+     PHONE   VARCHAR (10)  NOT NULL , 
+     EMAIL   VARCHAR (254)  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_CUST.CUST_ID IS 'Unique Identifier of Customer' 
+;
+
+COMMENT ON COLUMN AYT_CUST.FNAME IS 'First Name of the Customer' 
+;
+
+COMMENT ON COLUMN AYT_CUST.LNAME IS 'Last Name of the Customer' 
+;
+
+COMMENT ON COLUMN AYT_CUST.PHONE IS 'Phone Number of the Customer' 
+;
+
+COMMENT ON COLUMN AYT_CUST.EMAIL IS 'Email of the customer' 
+;
+
+ALTER TABLE AYT_CUST 
+    ADD CONSTRAINT AYT_CUST_PK PRIMARY KEY ( CUST_ID ) ;
+
+CREATE TABLE AYT_CUST_EXHIBITION 
+    ( 
+     REG_ID   BIGINT  NOT NULL , 
+     EVENT_ID BIGINT  NOT NULL , 
+     CUST_ID  INTEGER 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_CUST_EXHIBITION.REG_ID IS 'Registration ID of the exhibition' 
+;
+
+ALTER TABLE AYT_CUST_EXHIBITION 
+    ADD CONSTRAINT AYT_CUST_EXHIBITION_PK PRIMARY KEY ( REG_ID ) ;
+
+CREATE TABLE AYT_CUST_ID 
+    ( 
+     ID_NUM  VARCHAR (10)  NOT NULL , 
+     CUST_ID INTEGER  NOT NULL , 
+     ID_TYPE VARCHAR (10)  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_CUST_ID.ID_NUM IS 'Identification Number of the Customer for a Particular Identification Type' 
+;
+
+ALTER TABLE AYT_CUST_ID 
+    ADD CONSTRAINT AYT_CUST_ID_PK PRIMARY KEY ( ID_NUM, ID_TYPE, CUST_ID ) ;
+
+CREATE TABLE AYT_CUST_ROOM 
+    ( 
+     RES_ID     INTEGER  NOT NULL , 
+     START_TIME TIMESTAMP (0)  NOT NULL , 
+     END_TIME   TIMESTAMP (0)  NOT NULL , 
+     "DESC"     VARCHAR (100)  NOT NULL , 
+     NUM_INDV   SMALLINT  NOT NULL , 
+     CUST_ID    INTEGER , 
+     ROOM_ID    VARCHAR (4) 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_CUST_ROOM.RES_ID IS 'Unique Identifier of the Reservation.' 
+;
+
+COMMENT ON COLUMN AYT_CUST_ROOM.START_TIME IS 'Start Time of the Reservation' 
+;
+
+COMMENT ON COLUMN AYT_CUST_ROOM.END_TIME IS 'End Time of the Reservation' 
+;
+
+COMMENT ON COLUMN AYT_CUST_ROOM."DESC" IS 'Short Description of why the reservation is needed' 
+;
+
+COMMENT ON COLUMN AYT_CUST_ROOM.NUM_INDV IS 'Expected number of Individuals that will be in the room during the reservation.' 
+;
+
+ALTER TABLE AYT_CUST_ROOM 
+    ADD CONSTRAINT AYT_CUST_ROOM_PK PRIMARY KEY ( RES_ID ) ;
+
+CREATE TABLE AYT_EVENT 
+    ( 
+     EVENT_ID        BIGINT  NOT NULL , 
+     EVENT_NAME      VARCHAR (100)  NOT NULL , 
+     START_DATE_TIME TIMESTAMP(0)  NOT NULL , 
+     END_DATE_TIME   TIMESTAMP(0)  NOT NULL , 
+     EVENT_TOPIC     VARCHAR (30)  NOT NULL , 
+     EVENT_TYPE      VARCHAR (1)  NOT NULL 
+    ) 
+;
+
+ALTER TABLE AYT_EVENT 
+    ADD CONSTRAINT CH_INH_AYT_EVENT 
+    CHECK (EVENT_TYPE IN ('E', 'S')) 
+;
+
+COMMENT ON COLUMN AYT_EVENT.EVENT_ID IS 'Unique Identifier of the Event' 
+;
+
+COMMENT ON COLUMN AYT_EVENT.EVENT_NAME IS 'Name of the event' 
+;
+
+COMMENT ON COLUMN AYT_EVENT.START_DATE_TIME IS 'Start Date Time of the Event' 
+;
+
+COMMENT ON COLUMN AYT_EVENT.END_DATE_TIME IS 'End Date time of the event' 
+;
+
+COMMENT ON COLUMN AYT_EVENT.EVENT_TOPIC IS 'Topic of the Event' 
+;
+
+COMMENT ON COLUMN AYT_EVENT.EVENT_TYPE IS 'General Discriminator of the event' 
+;
+
+ALTER TABLE AYT_EVENT 
+    ADD CONSTRAINT AYT_EVENT_PK PRIMARY KEY ( EVENT_ID ) ;
+
+CREATE TABLE AYT_EXHIBITION 
+    ( 
+     EVENT_ID BIGINT  NOT NULL , 
+     EXPENSES DECIMAL (9,2)  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_EXHIBITION.EVENT_ID IS 'Unique Identifier of the Event' 
+;
+
+COMMENT ON COLUMN AYT_EXHIBITION.EXPENSES IS 'Total Expense spend in the Seminar' 
+;
+
+ALTER TABLE AYT_EXHIBITION 
+    ADD CONSTRAINT AYT_EXHIBITION_PK PRIMARY KEY ( EVENT_ID ) ;
+
+CREATE TABLE AYT_ID 
+    ( 
+     ID_TYPE VARCHAR (10)  NOT NULL , 
+     ID_NAME VARCHAR (30)  NOT NULL , 
+     "DESC"  VARCHAR (200)  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_ID.ID_TYPE IS 'Unique Identifier for the Identification Types' 
+;
+
+COMMENT ON COLUMN AYT_ID.ID_NAME IS 'Name of the Identification Types' 
+;
+
+COMMENT ON COLUMN AYT_ID."DESC" IS 'Description of the Identification Type' 
+;
+
+ALTER TABLE AYT_ID 
+    ADD CONSTRAINT AYT_ID_PK PRIMARY KEY ( ID_TYPE ) ;
+
+CREATE TABLE AYT_INVOICE 
+    ( 
+     INVOICE_ID   INT  NOT NULL , 
+     INVOICE_DATE TIMESTAMP(0)  NOT NULL , 
+     TOTAL_AMOUNT DECIMAL (6,2)  NOT NULL , 
+     RENTAL_ID    BIGINT  NOT NULL , 
+     PAY_ID       BIGINT  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_INVOICE.INVOICE_ID IS 'Unique Identifer of the Invoice' 
+;
+
+COMMENT ON COLUMN AYT_INVOICE.INVOICE_DATE IS 'Date the Invoice was generated' 
+;
+
+COMMENT ON COLUMN AYT_INVOICE.TOTAL_AMOUNT IS 'Total Due Amount for the invoice' 
+;
+CREATE UNIQUE INDEX AYT_INVOICE__IDX ON AYT_INVOICE 
+    ( 
+     RENTAL_ID ASC 
+    ) 
+;
+
+ALTER TABLE AYT_INVOICE 
+    ADD CONSTRAINT AYT_INVOICE_PK PRIMARY KEY ( INVOICE_ID ) ;
+
+CREATE TABLE AYT_PAY 
+    ( 
+     PAY_ID    BIGINT  NOT NULL , 
+     DATE_TIME TIMESTAMP(0)  NOT NULL , 
+     AMOUNT    DECIMAL (6,2)  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_PAY.PAY_ID IS 'Unique Identifier for Payment' 
+;
+
+COMMENT ON COLUMN AYT_PAY.DATE_TIME IS 'The datetime of the payment' 
+;
+
+COMMENT ON COLUMN AYT_PAY.AMOUNT IS 'Amount Paid' 
+;
+
+ALTER TABLE AYT_PAY 
+    ADD CONSTRAINT AYT_PAY_PK PRIMARY KEY ( PAY_ID ) ;
+
+CREATE TABLE AYT_PAY_TYPE 
+    ( 
+     TYPE_ID   SMALLINT  NOT NULL , 
+     TYPE_NAME VARCHAR (20)  NOT NULL , 
+     "DESC"    VARCHAR (200) 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_PAY_TYPE.TYPE_ID IS 'Unique Identifier of the Payment Type' 
+;
+
+COMMENT ON COLUMN AYT_PAY_TYPE.TYPE_NAME IS 'Name of the Payment Type. Is it credit card, debit card, etc.' 
+;
+
+COMMENT ON COLUMN AYT_PAY_TYPE."DESC" IS 'Description of the Payment Type' 
+;
+
+ALTER TABLE AYT_PAY_TYPE 
+    ADD CONSTRAINT AYT_PAY_TYPE_PK PRIMARY KEY ( TYPE_ID ) ;
+
+CREATE TABLE AYT_RENTAL 
+    ( 
+     RENTAL_ID     BIGINT  NOT NULL , 
+     BORROW_DATE   TIMESTAMP(0)  NOT NULL , 
+     EXPECTED_DATE TIMESTAMP(0)  NOT NULL , 
+     ACTUAL_DATE   TIMESTAMP(0) , 
+     CUST_ID       INTEGER , 
+     COPY_ID       INT  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_RENTAL.RENTAL_ID IS 'Unique Identifier for Rental' 
+;
+
+COMMENT ON COLUMN AYT_RENTAL.BORROW_DATE IS 'Borrow Date of the Book Rental' 
+;
+
+COMMENT ON COLUMN AYT_RENTAL.EXPECTED_DATE IS 'Expected date the book is to be returned' 
+;
+
+COMMENT ON COLUMN AYT_RENTAL.ACTUAL_DATE IS 'Actual Date when the book was returned' 
+;
+
+ALTER TABLE AYT_RENTAL 
+    ADD CONSTRAINT AYT_RENTAL_PK PRIMARY KEY ( RENTAL_ID ) ;
+
+CREATE TABLE AYT_ROOM 
+    ( 
+     ROOM_ID  VARCHAR (4)  NOT NULL , 
+     CAPACITY SMALLINT  NOT NULL , 
+     "DESC"   VARCHAR (100)  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_ROOM.ROOM_ID IS 'Unique Identifier of the Room. The ID shows which floor the room is on' 
+;
+
+COMMENT ON COLUMN AYT_ROOM.CAPACITY IS 'Capacity of the Room. Max Room capacity assumed to be 9' 
+;
+
+COMMENT ON COLUMN AYT_ROOM."DESC" IS 'Brief Description of the room' 
+;
+
+ALTER TABLE AYT_ROOM 
+    ADD CONSTRAINT AYT_ROOM_PK PRIMARY KEY ( ROOM_ID ) ;
+
+CREATE TABLE AYT_SEMINAR 
+    ( 
+     EVENT_ID        BIGINT  NOT NULL , 
+     KEYNOTE_SPEAKER VARCHAR (100) , 
+     SEMINAR_TYPE    VARCHAR (20)  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_SEMINAR.EVENT_ID IS 'Unique Identifier of the Event' 
+;
+
+COMMENT ON COLUMN AYT_SEMINAR.KEYNOTE_SPEAKER IS 'Name of the Keynote Speaker' 
+;
+
+COMMENT ON COLUMN AYT_SEMINAR.SEMINAR_TYPE IS 'What Type of Seminar is being hosted' 
+;
+
+ALTER TABLE AYT_SEMINAR 
+    ADD CONSTRAINT AYT_SEMINAR_PK PRIMARY KEY ( EVENT_ID ) ;
+
+CREATE TABLE AYT_SPONSOR 
+    ( 
+     SPONSOR_ID INT  NOT NULL , 
+     FNAME      VARCHAR (100)  NOT NULL , 
+     LNAME      VARCHAR (30) , 
+     EMAIL      VARCHAR (254)  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_SPONSOR.SPONSOR_ID IS 'Unique Identifier of Sponsor' 
+;
+
+COMMENT ON COLUMN AYT_SPONSOR.FNAME IS 'First Name of the Sponsor. If the sponsor is an organization, full name of the organization' 
+;
+
+COMMENT ON COLUMN AYT_SPONSOR.LNAME IS 'Last name of the sponsor. Empty if sponsor is an organization' 
+;
+
+COMMENT ON COLUMN AYT_SPONSOR.EMAIL IS 'Email of the sponsor' 
+;
+
+ALTER TABLE AYT_SPONSOR 
+    ADD CONSTRAINT AYT_SPONSOR_PK PRIMARY KEY ( SPONSOR_ID ) ;
+
+CREATE TABLE AYT_SPONSOR_SEMINAR 
+    ( 
+     AMOUNT     DECIMAL (10,2)  NOT NULL , 
+     EVENT_ID   BIGINT  NOT NULL , 
+     SPONSOR_ID INT  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_SPONSOR_SEMINAR.AMOUNT IS 'Amount Given by the sponsor for the seminar' 
+;
+
+ALTER TABLE AYT_SPONSOR_SEMINAR 
+    ADD CONSTRAINT AYT_SPONSOR_SEMINAR_PK PRIMARY KEY ( EVENT_ID, SPONSOR_ID ) ;
+
+CREATE TABLE AYT_TYPE_PAY 
+    ( 
+     INST_NUM  BIGINT  NOT NULL , 
+     CARD_NAME VARCHAR (100) , 
+     PAY_ID    BIGINT  NOT NULL , 
+     TYPE_ID   SMALLINT  NOT NULL 
+    ) 
+;
+
+COMMENT ON COLUMN AYT_TYPE_PAY.INST_NUM IS 'Instrumentation Number. If payment is done by cash, then INST_NUM will be 0.' 
+;
+
+COMMENT ON COLUMN AYT_TYPE_PAY.CARD_NAME IS 'Name in the Card. Optional since people can pay using cash' 
+;
+
+ALTER TABLE AYT_TYPE_PAY 
+    ADD CONSTRAINT AYT_TYPE_PAY_PK PRIMARY KEY ( PAY_ID, TYPE_ID ) ;
+
+ALTER TABLE AYT_AUTHOR_SEMINAR 
+    ADD CONSTRAINT AYT_AUT_SEM_AYT_AUT_FK FOREIGN KEY 
+    ( 
+     AUTH_ID
+    ) 
+    REFERENCES AYT_AUTHOR 
+    ( 
+     AUTH_ID
+    ) 
+;
+
+ALTER TABLE AYT_AUTHOR_SEMINAR 
+    ADD CONSTRAINT AYT_AUT_SEM_AYT_SEM_FK FOREIGN KEY 
+    ( 
+     EVENT_ID
+    ) 
+    REFERENCES AYT_SEMINAR 
+    ( 
+     EVENT_ID
+    ) 
+;
+
+ALTER TABLE AYT_BOOK_AUTHOR 
+    ADD CONSTRAINT AYT_BOOK_AUTHOR_AYT_AUTHOR_FK FOREIGN KEY 
+    ( 
+     AUTH_ID
+    ) 
+    REFERENCES AYT_AUTHOR 
+    ( 
+     AUTH_ID
+    ) 
+;
+
+ALTER TABLE AYT_BOOK_AUTHOR 
+    ADD CONSTRAINT AYT_BOOK_AUTHOR_AYT_BOOK_FK FOREIGN KEY 
+    ( 
+     BOOK_ID
+    ) 
+    REFERENCES AYT_BOOK 
+    ( 
+     BOOK_ID
+    ) 
+;
+
+ALTER TABLE AYT_BOOK_COPY 
+    ADD CONSTRAINT AYT_BOOK_COPY_AYT_BOOK_FK FOREIGN KEY 
+    ( 
+     BOOK_ID
+    ) 
+    REFERENCES AYT_BOOK 
+    ( 
+     BOOK_ID
+    ) 
+;
+
+ALTER TABLE AYT_CUST_EXHIBITION 
+    ADD CONSTRAINT AYT_CUST_EX_AYT_CUST_FK FOREIGN KEY 
+    ( 
+     CUST_ID
+    ) 
+    REFERENCES AYT_CUST 
+    ( 
+     CUST_ID
+    ) 
+;
+
+ALTER TABLE AYT_CUST_EXHIBITION 
+    ADD CONSTRAINT AYT_CUST_EX_AYT_EX_FK FOREIGN KEY 
+    ( 
+     EVENT_ID
+    ) 
+    REFERENCES AYT_EXHIBITION 
+    ( 
+     EVENT_ID
+    ) 
+;
+
+ALTER TABLE AYT_CUST_ID 
+    ADD CONSTRAINT AYT_CUST_ID_AYT_CUST_FK FOREIGN KEY 
+    ( 
+     CUST_ID
+    ) 
+    REFERENCES AYT_CUST 
+    ( 
+     CUST_ID
+    ) 
+;
+
+ALTER TABLE AYT_CUST_ID 
+    ADD CONSTRAINT AYT_CUST_ID_AYT_ID_FK FOREIGN KEY 
+    ( 
+     ID_TYPE
+    ) 
+    REFERENCES AYT_ID 
+    ( 
+     ID_TYPE
+    ) 
+;
+
+ALTER TABLE AYT_CUST_ROOM 
+    ADD CONSTRAINT AYT_CUST_ROOM_AYT_CUST_FK FOREIGN KEY 
+    ( 
+     CUST_ID
+    ) 
+    REFERENCES AYT_CUST 
+    ( 
+     CUST_ID
+    ) 
+;
+
+ALTER TABLE AYT_CUST_ROOM 
+    ADD CONSTRAINT AYT_CUST_ROOM_AYT_ROOM_FK FOREIGN KEY 
+    ( 
+     ROOM_ID
+    ) 
+    REFERENCES AYT_ROOM 
+    ( 
+     ROOM_ID
+    ) 
+;
+
+ALTER TABLE AYT_EXHIBITION 
+    ADD CONSTRAINT AYT_EXHIBITION_AYT_EVENT_FK FOREIGN KEY 
+    ( 
+     EVENT_ID
+    ) 
+    REFERENCES AYT_EVENT 
+    ( 
+     EVENT_ID
+    ) 
+;
+
+ALTER TABLE AYT_INVOICE 
+    ADD CONSTRAINT AYT_INVOICE_AYT_PAY_FK FOREIGN KEY 
+    ( 
+     PAY_ID
+    ) 
+    REFERENCES AYT_PAY 
+    ( 
+     PAY_ID
+    ) 
+;
+
+ALTER TABLE AYT_INVOICE 
+    ADD CONSTRAINT AYT_INVOICE_AYT_RENTAL_FK FOREIGN KEY 
+    ( 
+     RENTAL_ID
+    ) 
+    REFERENCES AYT_RENTAL 
+    ( 
+     RENTAL_ID
+    ) 
+;
+
+ALTER TABLE AYT_RENTAL 
+    ADD CONSTRAINT AYT_RENTAL_AYT_BOOK_COPY_FK FOREIGN KEY 
+    ( 
+     COPY_ID
+    ) 
+    REFERENCES AYT_BOOK_COPY 
+    ( 
+     COPY_ID
+    ) 
+;
+
+ALTER TABLE AYT_RENTAL 
+    ADD CONSTRAINT AYT_RENTAL_AYT_CUST_FK FOREIGN KEY 
+    ( 
+     CUST_ID
+    ) 
+    REFERENCES AYT_CUST 
+    ( 
+     CUST_ID
+    ) 
+;
+
+ALTER TABLE AYT_SEMINAR 
+    ADD CONSTRAINT AYT_SEMINAR_AYT_EVENT_FK FOREIGN KEY 
+    ( 
+     EVENT_ID
+    ) 
+    REFERENCES AYT_EVENT 
+    ( 
+     EVENT_ID
+    ) 
+;
+
+ALTER TABLE AYT_SPONSOR_SEMINAR 
+    ADD CONSTRAINT AYT_SP_SEM_AYT_SEM_FK FOREIGN KEY 
+    ( 
+     EVENT_ID
+    ) 
+    REFERENCES AYT_SEMINAR 
+    ( 
+     EVENT_ID
+    ) 
+;
+
+ALTER TABLE AYT_SPONSOR_SEMINAR 
+    ADD CONSTRAINT AYT_SP_SEM_AYT_SP_FK FOREIGN KEY 
+    ( 
+     SPONSOR_ID
+    ) 
+    REFERENCES AYT_SPONSOR 
+    ( 
+     SPONSOR_ID
+    ) 
+;
+
+ALTER TABLE AYT_TYPE_PAY 
+    ADD CONSTRAINT AYT_TYPE_PAY_AYT_PAY_FK FOREIGN KEY 
+    ( 
+     PAY_ID
+    ) 
+    REFERENCES AYT_PAY 
+    ( 
+     PAY_ID
+    ) 
+;
+
+ALTER TABLE AYT_TYPE_PAY 
+    ADD CONSTRAINT AYT_TYPE_PAY_AYT_PAY_TYPE_FK FOREIGN KEY 
+    ( 
+     TYPE_ID
+    ) 
+    REFERENCES AYT_PAY_TYPE 
+    ( 
+     TYPE_ID
+    ) 
+;
+
+CREATE OR REPLACE FUNCTION ARC_FKArc_3_AYT_SEMINAR_func() RETURNS TRIGGER AS $$  
+DECLARE 
+    d VARCHAR (1); 
+BEGIN 
+    SELECT A.EVENT_TYPE INTO d 
+    FROM AYT_EVENT A 
+    WHERE A.EVENT_ID = NEW.EVENT_ID; 
+    IF (d IS NULL OR d <> 'S') THEN 
+        raise exception '%s', 'FK AYT_SEMINAR_AYT_EVENT_FK in Table AYT_SEMINAR violates Arc constraint on Table AYT_EVENT - discriminator column EVENT_TYPE doesn''t have value ''S''' using errcode = -20223; 
+    END IF; 
+    EXCEPTION 
+    WHEN NO_DATA_FOUND THEN 
+        NULL; 
+    WHEN OTHERS THEN 
+        RAISE; 
+END; 
+
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER ARC_FKArc_3_AYT_SEMINAR 
+BEFORE INSERT OR UPDATE OF EVENT_ID 
+ON AYT_SEMINAR 
+FOR EACH ROW
+EXECUTE FUNCTION ARC_FKArc_3_AYT_SEMINAR_func();
+
+CREATE OR REPLACE FUNCTION ARC_FKArc_3_AYT_EXHIBITION_func() RETURNS TRIGGER AS $$  
+DECLARE 
+    d VARCHAR (1); 
+BEGIN 
+    SELECT A.EVENT_TYPE INTO d 
+    FROM AYT_EVENT A 
+    WHERE A.EVENT_ID = NEW.EVENT_ID; 
+    IF (d IS NULL OR d <> 'E') THEN 
+        raise exception '%s', 'FK AYT_EXHIBITION_AYT_EVENT_FK in Table AYT_EXHIBITION violates Arc constraint on Table AYT_EVENT - discriminator column EVENT_TYPE doesn''t have value ''E''' using errcode = -20223; 
+    END IF; 
+    EXCEPTION 
+    WHEN NO_DATA_FOUND THEN 
+        NULL; 
+    WHEN OTHERS THEN 
+        RAISE; 
+END; 
+
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER ARC_FKArc_3_AYT_EXHIBITION 
+BEFORE INSERT OR UPDATE OF EVENT_ID 
+ON AYT_EXHIBITION 
+FOR EACH ROW
+EXECUTE FUNCTION ARC_FKArc_3_AYT_EXHIBITION_func();
+
+
+
+-- SQLINES DEMO *** per Data Modeler Summary Report: 
+-- 
+-- SQLINES DEMO ***                        21
+-- SQLINES DEMO ***                         1
+-- SQLINES DEMO ***                        43
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO *** DY                      0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         2
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***  TYPE                   0
+-- SQLINES DEMO ***  TYPE                   0
+-- SQLINES DEMO ***  TYPE BODY              0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO *** EGMENT                  0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO *** ED VIEW                 0
+-- SQLINES DEMO *** ED VIEW LOG             0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         0
+-- 
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         0
+-- 
+-- SQLINES DEMO ***                         0
+-- 
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO *** A                       0
+-- SQLINES DEMO *** T                       0
+-- 
+-- SQLINES DEMO ***                         0
+-- SQLINES DEMO ***                         0
