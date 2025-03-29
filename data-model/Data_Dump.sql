1e78a37b0e774b24dd8dee418caff41609e32e51
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-03-29 16:42:29 -0400

    Add initial constraints
    Tested the Room Capacity contraint

---
+++ data-model/Data_Dump.sql
@@ -0,0 +1,7 @@
+-- Room Data
+insert into AYT_ROOM(room_id, capacity, descr) values
+('100', 10, 'Sample Room');
+
+-- Customer Data Insert
+insert into ayt_cust(FNAME, LNAME, PHONE, EMAIL) values
+('skldfj', 'asdfa', '1231231231', 'asdlkfj@adsklf.com');
---
+++ data-model/Postgres_agument.sql
@@ -0,0 +1,22 @@
+
+-- Checking if the number of Individuals exceed the room capacity or not
+create or replace function room_cap() returns trigger as $room_cap$
+declare
+    cap smallint;
+begin
+    -- Get the room capacity
+    select capacity into cap
+    from AYT_ROOM
+    where room_id = new.room_id
+    limit 1;
+
+    if (new.NUM_INDV > cap) then
+        raise exception 'The Number of Individuals exceed the room capacity';
+    end if;
+end;
+$room_cap$ language plpgsql;
+
+create or replace trigger check_room_cap
+before insert or update on AYT_CUST_ROOM
+    for each row execute procedure room_cap();
+
