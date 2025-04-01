59e9b7c4571cb771ec9ce48f6bb84d041c5af431
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-03-31 23:23:17 -0400

    Add Extra Unique Constraints

--- data-model/Postgres_Unique_Constraints.sql
+++ data-model/Postgres_Unique_Constraints.sql
@@ -2,6 +2,10 @@ ALTER TABLE ayt_cust add constraint cust_email_unique UNIQUE(email);
 
 ALTER TABLE ayt_cust add constraint cust_phone_unique UNIQUE(phone);
 
+ALTER TABLE ayt_cust_id add constraint cust_id_num_unique UNIQUE(id_num);
+
 ALTER TABLE ayt_author add constraint auth_email_unique UNIQUE(email);
 
 ALTER TABLE ayt_book add constraint book_isbn_unique UNIQUE(ISBN);
+
+ALTER TABLE ayt_sponsor add constraint sponsor_email_unique UNIQUE(email);
