587e3f7ed7b58e4147ad114c0526617289286fa7
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-04-09 02:34:07 -0400

    Create user model with roles
    Added admin, staff, customer roles to the BaseUser table

--- be/server/user/models.py
+++ be/server/user/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import (
+    AbstractUser,
+)
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICE = [
+        ("admin", "Admin"),
+        ("staff", "Staff"),
+        ("customer", "Customer"),
+    ]
+
+    role = models.CharField(max_length=10, choices=ROLE_CHOICE, default="customer")
