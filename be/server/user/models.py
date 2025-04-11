d6abfc6e79b9253c172d54a5c3b97010944b75c4
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-04-11 03:28:03 -0400

    Modify UserManager to modify createsupersuer command
    Create super user command now defaults user role to admin

--- be/server/user/models.py
+++ be/server/user/models.py
@@ -1,10 +1,36 @@
-from django.contrib.auth.models import (
-    AbstractUser,
-)
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-
 # Create your models here.
+
+
+# Created a CustomeUserManager to add the default role of admin
+# for Super User
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **kwargs):
+        kwargs.setdefault("is_staff", True)
+        kwargs.setdefault("is_superuser", True)
+        kwargs.setdefault("role", "admin")
+
+        if kwargs.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if kwargs.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+        if kwargs.get("role") != "admin":
+            raise ValueError("Superuser must have role=Admin.")
+
+        return self.create_user(email, password, **kwargs)
+
+
 class User(AbstractUser):
     ROLE_CHOICE = [
         ("admin", "Admin"),
@@ -15,3 +41,4 @@ class User(AbstractUser):
     role = models.CharField(max_length=10, choices=ROLE_CHOICE, default="customer")
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=150, unique=True)
+    objects = CustomUserManager()
