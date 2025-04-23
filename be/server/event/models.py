c4e3dd93a71fbb02789f582272ae95b8dbb92b62
sansha <yumikot2319@gmail.com>
2025-04-23 16:04:32 -0400

    event and room models.py added

--- be/server/event/models.py
+++ be/server/event/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    event_id = models.IntegerField(primary_key=True)
+    event_name = models.CharField(max_length=100)
+    start_date_time = models.DateTimeField()
+    end_date_time = models.DateTimeField()
+    event_topic = models.CharField(max_length=30)
+    event_type = models.CharField(max_length=1)
+
+    class Meta:
+        db_table = 'AYT_EVENT'
+
+    def __str__(self):
+        return self.event_name
+
+"""
+CREATE TABLE AYT_EVENT 
+    ( 
+     EVENT_ID        NUMBER (10)  NOT NULL , 
+     EVENT_NAME      VARCHAR2 (100)  NOT NULL , 
+     START_DATE_TIME DATE  NOT NULL , 
+     END_DATE_TIME   DATE  NOT NULL , 
+     EVENT_TOPIC     VARCHAR2 (30)  NOT NULL , 
+     EVENT_TYPE      VARCHAR2 (1)  NOT NULL 
+    ) 
+;
+"""
--- be/server/room/models.py
+++ be/server/room/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    room_id = models.CharField(max_length=4, primary_key=True)
+    capacity = models.IntegerField()
+    descr = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'AYT_ROOM'
+
+    def __str__(self):
+        return self.descr
+
+
+"""
+CREATE TABLE AYT_ROOM 
+    ( 
+     ROOM_ID  VARCHAR2 (4)  NOT NULL , 
+     CAPACITY NUMBER (1)  NOT NULL , 
+     DESCR    VARCHAR2 (100)  NOT NULL 
+    ) 
+;
+"""
