eae9a5c1ef22ef34d7a863c0b8b86bc6764b8e4f
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-04-29 17:51:04 -0400

    Remove redundant urls
    Remove default TokenPair url paths

--- be/server/server/urls.py
+++ be/server/server/urls.py
@@ -18,10 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -31,8 +31,8 @@ from drf_spectacular.views import (
 
 api_patterns = [
     # JWT Token Routes
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    # path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    # path("token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
     # OpenAPI Routes
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
