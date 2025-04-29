179cf0d3bc32f5865fece56c4ea3d8f87eb51add
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-04-29 17:48:26 -0400

    Implement Custom Cookie JWT Token Authentication class

---
+++ be/server/user/authentication.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from rest_framework.views import Request
+from rest_framework_simplejwt.authentication import JWTAuthentication, AuthUser
+from rest_framework_simplejwt.tokens import Token
+from rest_framework_simplejwt.exceptions import AuthenticationFailed
+
+
+class CookieJWTAuthentication(JWTAuthentication):
+    def authenticate(self, request: Request) -> Optional[tuple[AuthUser, Token]]:
+        token = request.COOKIES.get("access_token")
+
+        if not token:
+            return None
+
+        try:
+            validated_token = self.get_validated_token(token)
+        except AuthenticationFailed as e:
+            raise AuthenticationFailed(f"Invalid Token: {str(e)}")
+
+        try:
+            user = self.get_user(validated_token)
+            return user, validated_token
+        except AuthenticationFailed as e:
+            raise AuthenticationFailed(f"Invalid Token: {str(e)}")
+        # return super().authenticate(request)
