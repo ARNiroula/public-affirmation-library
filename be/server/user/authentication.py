81d8978257ed3eb91c83e9e80c9c797739a08bf9
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-05-06 16:40:40 -0400

    Add OpenAPI Specs for CookieJWTAuthentication

--- be/server/user/authentication.py
+++ be/server/user/authentication.py
@@ -4,6 +4,19 @@ from rest_framework.views import Request
 from rest_framework_simplejwt.authentication import JWTAuthentication, AuthUser
 from rest_framework_simplejwt.tokens import Token
 from rest_framework_simplejwt.exceptions import AuthenticationFailed
+from drf_spectacular.extensions import OpenApiAuthenticationExtension
+
+
+class CookieJWTAuthenticationExtension(OpenApiAuthenticationExtension):
+    target_class = "user.authentication.CookieJWTAuthentication"  # your path
+    name = "CookieJWTAuth"  # any unique name
+
+    def get_security_definition(self, auto_schema):
+        return {
+            "type": "apiKey",
+            "in": "cookie",
+            "name": "access_token",
+        }
 
 
 class CookieJWTAuthentication(JWTAuthentication):
