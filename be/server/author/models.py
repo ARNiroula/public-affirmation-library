8050c7b089342236ce025cd1da34ae018a465f44
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-05-06 22:58:37 -0400

    Reformat Files

--- be/server/author/models.py
+++ be/server/author/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Author(models.Model):
     auth_id = models.IntegerField(primary_key=True)
     fname = models.CharField(max_length=30)
@@ -12,24 +13,24 @@ class Author(models.Model):
     email = models.EmailField(max_length=254, blank=True, null=True)
 
     class Meta:
-        db_table = 'AYT_AUTHOR'
+        db_table = "AYT_AUTHOR"
 
     def __str__(self):
         return f"{self.fname} {self.lname}"
-    
+
 
 """
 CREATE TABLE AYT_AUTHOR 
-    ( 
-     AUTH_ID    INT  NOT NULL , 
-     FNAME      VARCHAR (30)  NOT NULL , 
-     MNAME      VARCHAR (10) , 
-     LNAME      VARCHAR (30) , 
-     ADDR_ST    VARCHAR (100) , 
-     ADDR_CITY  VARCHAR (100) , 
-     ADDR_STATE VARCHAR (100) , 
-     ADDR_CTRY  VARCHAR (100) , 
+    (
+     AUTH_ID    INT  NOT NULL ,
+     FNAME      VARCHAR (30)  NOT NULL ,
+     MNAME      VARCHAR (10) ,
+     LNAME      VARCHAR (30) ,
+     ADDR_ST    VARCHAR (100) ,
+     ADDR_CITY  VARCHAR (100) ,
+     ADDR_STATE VARCHAR (100) ,
+     ADDR_CTRY  VARCHAR (100) ,
      EMAIL      VARCHAR (254) 
-    ) 
+    )
 ;
 """
