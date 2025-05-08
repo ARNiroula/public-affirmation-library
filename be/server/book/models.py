7b1943dd7cf2efd336ca7b8e9308e7384f8b048a
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-05-08 06:54:40 -0400

    Modify Book fields

--- be/server/book/models.py
+++ be/server/book/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
+from utils.enums import Topic
+
 
 class Book(models.Model):
     book_id = models.AutoField(primary_key=True)
     isbn = models.CharField(max_length=13)
     name = models.CharField(max_length=200)
-    topic = models.CharField(max_length=50)
+    topic = models.CharField(
+        max_length=50,
+        choices=Topic,
+        default=Topic.FICTION,
+    )
     summary = models.CharField(max_length=50, default="Lorem Ipsum")
-    pub_date = models.DateTimeField()
+    pub_date = models.DateField(null=True, blank=True)
     cover_url = models.URLField(null=True, blank=True)
 
     class Meta:
