19afadb68ebe3de80da554eecd095e47800481be
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-05-08 06:53:17 -0400

    Fix BookCopy Import

--- be/server/rental_system/models.py
+++ be/server/rental_system/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models import CheckConstraint, Q, F
 
 from user.models import User
-from book.models import BookCopy
+from book_copy.models import BookCopy
 
 
 class Rental(models.Model):
