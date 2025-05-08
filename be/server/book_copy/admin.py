ea362f72c846e8ad545d0ceb2d7ed0dcf9de129c
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-05-08 06:53:45 -0400

    Modify Book Copy to show status

--- be/server/book_copy/admin.py
+++ be/server/book_copy/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import BookCopy
+
+
 # Register your models here.
+admin.site.register(BookCopy)
--- be/server/book_copy/models.py
+++ be/server/book_copy/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 from book.models import Book
 
+
 class BookCopy(models.Model):
-    copy_id = models.CharField(max_length=8, primary_key=True)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='copies')
+    STATUS_CHOICES = [
+        ("available", "Available"),
+        ("on_loan", "On Loan"),
+        ("reserved", "Reserved"),
+    ]
+
+    copy_id = models.AutoField(primary_key=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="copies")
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default="available"
+    )
