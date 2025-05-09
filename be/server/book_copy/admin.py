f55c1b2bd012a4c827f9d89a2475aea2489f299d
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-05-09 15:21:15 -0400

    Modify Book Copy Admin Class

--- be/server/book_copy/admin.py
+++ be/server/book_copy/admin.py
@@ -4,4 +4,26 @@ from .models import BookCopy
 
 
 # Register your models here.
-admin.site.register(BookCopy)
+@admin.register(BookCopy)
+class BookCopyAdmin(admin.ModelAdmin):
+    list_display = ("copy_id", "book__name", "status", "last_booked", "when_returned")
+    readonly_fields = (
+        "last_booked",
+        "when_returned",
+    )
+    list_filter = ("status",)
+
+    def last_booked(self, obj):
+        query = obj.rentals.order_by("-borrow_date").first()
+        if not query:
+            return ""
+        return query.borrow_date
+
+    def when_returned(self, obj):
+        query = obj.rentals.order_by("-borrow_date").first()
+        if not query:
+            return ""
+        return "Not Returned" if not query.actual_date else query.actual_date
+
+
+# admin.site.register(BookCopy)
