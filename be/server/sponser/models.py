e801fae2bddbf499d4cb77048503c775dfbe59e0
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-05-09 19:21:24 -0400

    Make Primary Key AutoIncrement Sponsor

--- be/server/sponser/models.py
+++ be/server/sponser/models.py
@@ -4,7 +4,7 @@ from seminar.models import Seminar
 
 
 class Sponser(models.Model):
-    sponser_id = models.IntegerField(max_length=5, primary_key=True)
+    sponser_id = models.AutoField(max_length=5, primary_key=True)
     fname = models.CharField(max_length=100)
     lname = models.CharField(max_length=30)
     email = models.CharField(max_length=254)
