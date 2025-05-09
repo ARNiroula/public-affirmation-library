393cbd8e28e36f5e4f1a22c8bcc98fb5458c8e41
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-05-09 19:15:46 -0400

    Fix Sponsor model and Add sponsor to admin site

--- be/server/sponser/admin.py
+++ be/server/sponser/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Sponser, SponserSeminarRelationship
 # Register your models here.
+
+admin.site.register(Sponser)
+admin.site.register(SponserSeminarRelationship)
--- be/server/sponser/models.py
+++ be/server/sponser/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 from seminar.models import Seminar
 
+
 class Sponser(models.Model):
-    sponser_id = models.IntegerField(max_legth=5, primary_key=True)
+    sponser_id = models.IntegerField(max_length=5, primary_key=True)
     fname = models.CharField(max_length=100)
     lname = models.CharField(max_length=30)
     email = models.CharField(max_length=254)
@@ -14,12 +15,15 @@ class Sponser(models.Model):
     def __str__(self):
         return self.name
 
+
 class SponserSeminarRelationship(models.Model):
-    amount = models.IntegerField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     sponser = models.ForeignKey(Sponser, on_delete=models.CASCADE)
     seminar = models.ForeignKey(Seminar, on_delete=models.CASCADE)
 
     class Meta:
         db_table = "AYT_SPONSER_SEMINAR_RELATIONSHIP"
 
+
 # number datatype is IntegerField.
+
