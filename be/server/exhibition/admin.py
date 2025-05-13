578adb738f111a1ac58c791f00adf52b84a0ca61
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-05-13 01:58:48 -0400

    Fix Exhibition Model and Admin views

--- be/server/exhibition/admin.py
+++ be/server/exhibition/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Exhibition
+from event.admin import EventAdminForm
+
+
+class ExhibitionAdminForm(EventAdminForm):
+    class Meta:  # pyright: ignore
+        model = Exhibition
+        fields = "__all__"
+
+
+@admin.register(Exhibition)
+class ExhibitionAdmin(admin.ModelAdmin):
+    form = ExhibitionAdminForm
+    list_display = (
+        "event_name",
+        "start_date_time",
+        "end_date_time",
+        "expenses",
+        "total_signed",
+        "event_topic",
+    )
+    readonly_fields = [
+        "image_preview",
+        "total_signed",
+    ]
+    list_filter = (
+        "start_date_time",
+        "event_topic",
+        "expenses",
+    )
+    search_fields = ("event_name",)
+
+    def image_preview(self, obj):
+        if obj.cover_url:
+            return format_html(
+                '<img src="{}" style="max-height: 200px;"/>', obj.cover_url
+            )
+        return "No image available"
+
+    image_preview.short_description = "Current Image"  # pyright: ignore
+
+    def total_signed(self, obj):
+        return obj.users.count()
+
+    total_signed.short_description = "People Signed"  # pyright: ignore
--- be/server/exhibition/models.py
+++ be/server/exhibition/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 from event.models import Event
 
-class Exhibition(models.Model):
-    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='exhibitions')
-    expenses = models.DecimalField(max_digits=9, decimal_places=2)
 
-    class Meta:
+# Create your models here.
+class Exhibition(Event):
+    expenses = models.DecimalField(max_digits=9, decimal_places=2)
+    users = models.ManyToManyField("user.User", related_name="exhibition")
+
+    class Meta:  # pyright: ignore
         db_table = "AYT_EXHIBITION"
 
     def __str__(self):
-        return self.exhibition_name
-
-# Create your models here.
+        return f"Exhibition {self.event_name}"
