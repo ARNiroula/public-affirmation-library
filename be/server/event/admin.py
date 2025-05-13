0aa40334b27e4f3c76264d27a5ccbddc17ef44e5
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-05-13 01:58:21 -0400

    Made Event Type Abstract Model

--- be/server/event/admin.py
+++ be/server/event/admin.py
@@ -1,3 +1,43 @@
-from django.contrib import admin
+import uuid
+
+from django import forms
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+# from django.contrib import admin
+from django.conf import settings
+from utils.storage import minio_client
 
 # Register your models here.
+
+
+class EventAdminForm(forms.ModelForm):
+    image_file = forms.FileField(required=False, label="Upload Image")
+
+    class Meta:
+        fields = "__all__"
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        image = self.files.get("image_file")
+        if image and isinstance(image, InMemoryUploadedFile):
+            # Generate a unique filename
+            ext = image.name.split(".")[-1]
+            filename = f"public/book/{uuid.uuid4()}.{ext}"
+            bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+
+            # Upload to MinIO
+            minio_client.upload_fileobj(
+                Fileobj=image,
+                Bucket=bucket_name,
+                Key=filename,
+                ExtraArgs={"ContentType": image.content_type},
+            )
+
+            # Construct the full URL
+            file_url = f"{minio_client.meta.endpoint_url}/{settings.AWS_STORAGE_BUCKET_NAME}/{filename}"
+            instance.image_url = file_url
+
+        instance.save()
+
+        return instance
--- be/server/event/models.py
+++ be/server/event/models.py
@@ -1,50 +1,63 @@
+from datetime import datetime, timedelta
+
 from django.db import models
 from django.db.models import Q, F
+
 from utils.enums import EventTopic
 
+
 class Event(models.Model):
-    event_id = models.IntegerField(primary_key=True)
-    event_name = models.CharField(max_length=100, db_index=True)
-    start_date_time = models.DateTimeField()
-    end_date_time = models.DateTimeField()
+    event_id = models.AutoField(primary_key=True)
+    event_name = models.CharField(max_length=100, db_index=True, default="Test")
+    start_date_time = models.DateTimeField(default=datetime.now())
+    end_date_time = models.DateTimeField(default=datetime.now() + timedelta(days=10))
+    image_url = models.URLField(null=True, blank=True)
 
-    event_topic = models.CharField(max_length=30)
-    topic_bitmap = models.BigIntegerField(default=0)
-
-    event_type = models.CharField(max_length=1)
+    event_topic = models.CharField(
+        max_length=30,
+        choices=EventTopic,
+        default=EventTopic.LITERATURE,
+        db_index=True,
+    )
+    topic_bitmap = models.BigIntegerField(default=0, editable=False, db_index=True)  # pyright: ignore
 
     class Meta:
-        db_table = "AYT_EVENT"
+        abstract = True
         constraints = [
             models.CheckConstraint(
-                name = "check_datetime_order",
-                check = Q(end_date_time__gt=F("start_date_time")),
+                name="check_datetime_order",
+                check=Q(end_date_time__gt=F("start_date_time")),
             ),
         ]
 
     def set_topics(self, topic_codes):
-        self.event_topics_bitmap = sum(1 << EventTopic.bit_position(code) for code in topic_codes)
+        self.event_topics_bitmap = sum(
+            1 << EventTopic.bit_position(code) for code in topic_codes
+        )
 
     def get_topics(self):
-        return [topic for topic in EventTopic if self.event_topics_bitmap & (1 << EventTopic.bit_position(topic.value))]
+        return [
+            topic
+            for topic in EventTopic
+            if self.event_topics_bitmap & (1 << EventTopic.bit_position(topic.value))
+        ]
 
     def has_topic(self, code):
         return bool(self.event_topics_bitmap & (1 << EventTopic.bit_position(code)))
 
     def __str__(self):
-        return self.event_name
+        return str(self.event_name)
 
 
 """
-CREATE TABLE AYT_EVENT 
-    ( 
-     EVENT_ID        NUMBER (10)  NOT NULL , 
-     EVENT_NAME      VARCHAR2 (100)  NOT NULL , 
-     START_DATE_TIME DATE  NOT NULL , 
-     END_DATE_TIME   DATE  NOT NULL , 
-     EVENT_TOPIC     VARCHAR2 (30)  NOT NULL , 
-     EVENT_TYPE      VARCHAR2 (1)  NOT NULL 
-    ) 
+CREATE TABLE AYT_EVENT
+    (
+     EVENT_ID        NUMBER (10)  NOT NULL ,
+     EVENT_NAME      VARCHAR2 (100)  NOT NULL ,
+     START_DATE_TIME DATE  NOT NULL ,
+     END_DATE_TIME   DATE  NOT NULL ,
+     EVENT_TOPIC     VARCHAR2 (30)  NOT NULL ,
+     EVENT_TYPE      VARCHAR2 (1)  NOT NULL
+    )
 ;
 """
-
