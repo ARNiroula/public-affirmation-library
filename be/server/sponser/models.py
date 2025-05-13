b7c91c44874db1ce9ee9b836a60f093e3826485b
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-05-13 01:59:49 -0400

    Fix Sponsor model

--- be/server/sponser/models.py
+++ be/server/sponser/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from seminar.models import Seminar
-
 
 class Sponser(models.Model):
     TYPE = (
@@ -11,20 +9,20 @@ class Sponser(models.Model):
     sponser_id = models.AutoField(max_length=5, primary_key=True)
     fname = models.CharField(max_length=100)
     lname = models.CharField(max_length=30, blank=True, null=True)
-    email = models.CharField(max_length=254)
+    email = models.CharField(max_length=254, unique=True)
     sponsor_type = models.CharField(max_length=1, choices=TYPE)
 
     class Meta:
         db_table = "AYT_SPONSER"
 
     def __str__(self):
-        return self.fname
+        return f"{self.fname} {self.email}"
 
 
 class SponserSeminarRelationship(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     sponser = models.ForeignKey(Sponser, on_delete=models.CASCADE)
-    seminar = models.ForeignKey(Seminar, on_delete=models.CASCADE)
+    seminar = models.ForeignKey("seminar.Seminar", on_delete=models.CASCADE)
 
     class Meta:
         db_table = "AYT_SPONSER_SEMINAR_RELATIONSHIP"
